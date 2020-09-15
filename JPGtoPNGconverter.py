3c30f66e409f485761ef68b958b10180342059a8
asiersalvo <65555472+asiersalvo@users.noreply.github.com>
2020-09-15 19:37:02 +0200

    Add files via upload

--- JPGtoPNGconverter.py
+++ JPGtoPNGconverter.py
@@ -6,11 +6,13 @@ folder_from = sys.argv[1]
 folder_to = sys.argv[2]
 # print(folder_from)
 # print(folder_to)
+
 #2 Tenemos que ver si la carpeta donde introducir las nuevas imágenes existe (la primera vez no) y si eso crearla y sino no.
-if os.path.isdir(folder_to):
-    print(f'La carpeta {folder_to} existe.')
-else:
+if not os.path.isdir(folder_to):
+#aquí también podríamos haber usado os.path.exists()
     os.mkdir(f'{folder_to}')
+#también podríamos haber usado os.makedirs()
+
 #3 Loop dentro de la carpeta con las imágenes y convirtiendo
 current = os.getcwd()
 #print(current)
@@ -18,6 +20,6 @@ entries = os.listdir(folder_from)
 for i in entries:
     all_name = os.path.basename(i)
     name = os.path.splitext(i)[0]
-    img = Image.open(f'./pokedex/{all_name}')
+    img = Image.open(f'./{folder_from}/{all_name}')
     img.save(f'./{folder_to}/{name}.png','png')
 
